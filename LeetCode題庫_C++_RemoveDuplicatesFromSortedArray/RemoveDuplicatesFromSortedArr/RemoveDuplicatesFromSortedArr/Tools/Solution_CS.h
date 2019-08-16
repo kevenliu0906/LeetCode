a485229f16513dd1e448ef2ab35983e66e85eef6
keven0906 <keven0906@gmail.com>
2019-08-16 17:47:59 +0800

    Refactor the project of "RemoveDuplicatesFromSortedArray" and reduce test time from 332ms to 156ms in Leetcode.

--- LeetCode題庫_C++_RemoveDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArr/RemoveDuplicatesFromSortedArr/Tools/Solution_CS.h
+++ LeetCode題庫_C++_RemoveDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArr/RemoveDuplicatesFromSortedArr/Tools/Solution_CS.h
@@ -6,26 +6,26 @@ class Solution
 {
 public:
 	int removeDuplicates(vector<int>& nums) {
-		bool   RemoveElement_fg = true;
-		do {
-			RemoveElement_fg = false;
-			for (size_t i_i = 0; i_i < nums.size(); i_i++)
+
+		for (size_t i_i = 0; i_i < nums.size(); i_i++)
+		{
+			if (i_i + 1 < nums.size())
 			{
-				for (size_t j_i = i_i + 1; j_i < nums.size(); j_i++)
+				if (nums[i_i] != nums[i_i + 1])
+					continue;
+			}
+			for (size_t j_i = i_i + 1; j_i < nums.size(); j_i++)
+			{
+				if (nums[i_i] == nums[j_i])
 				{
-					if (nums[i_i] == nums[j_i])
-					{
-						RemoveElement_fg = true;
-						nums.erase(begin(nums) + j_i);
-						break;
-					}
-					else
-						break;
+					nums.erase(begin(nums) + j_i);
+					j_i--;
+					continue;
 				}
-				if (RemoveElement_fg)
+				else  //因為輸入vector是有sort過，所以只要資料沒有相同就可以不用比對這輪，直接跳下一輪比對
 					break;
 			}
-		} while (RemoveElement_fg);
+		}
 
 		return (nums.size());
 	}
