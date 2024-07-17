5046a6ce399388dabbc956357121a2dba935ee9e
keven.liu <keven.liu@adlinktech.com>
2024-07-17 16:25:18 +0800

    [Change] Remodify the best solution for project "3Sum" with C++ solution.

--- LeetCode題庫_C++_15.3Sum/ConsoleApplication_3Sum/ConsoleApplication_3Sum/ConsoleApplication_3Sum.cpp
+++ LeetCode題庫_C++_15.3Sum/ConsoleApplication_3Sum/ConsoleApplication_3Sum/ConsoleApplication_3Sum.cpp
@@ -14,120 +14,120 @@ class Solution{
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
 
-        // // solution 1 (best)
-        // {
-        //     vector<vector<int>> result;  // final result
-
-        //     // sort the original array first
-        //     sort(nums.begin(), nums.end());
-
-        //     int max_len = nums.size();
-        //     for (size_t i = 0; i <= max_len - 3; i++)
-        //     {
-        //         int num = nums[i];
-        //         int target = 0 - num;
-        //         int left_index = i + 1;
-        //         int right_index = max_len - 1;
-
-        //         // skip the duplicated-checked element
-        //         if( (i > 0) && nums[i] == nums[i-1] )
-        //             continue;
-
-        //         while( left_index < right_index ){
-        //             if ( nums[left_index] + nums[right_index] == target )
-        //             {
-        //                 result.push_back({nums[i], nums[left_index], nums[right_index]});
-                        
-        //                 // skip the duplicated-checked element
-        //                 while( (left_index + 1 < max_len) && (nums[left_index] == nums[left_index + 1]) )
-        //                     left_index++;
-        //                 // skip the duplicated-checked element
-        //                 while( (right_index > 0) && nums[right_index] == nums[right_index - 1] )
-        //                     right_index--;
-
-        //                 left_index++;
-        //                 right_index--;
-        //             }
-        //             else if ( nums[left_index] + nums[right_index] < target )
-        //             {
-        //                 // skip the duplicated-checked element
-        //                 while( (left_index + 1 < max_len) && (nums[left_index] == nums[left_index + 1]) )
-        //                     left_index++;
-        //                 left_index++;
-        //             }
-        //             else
-        //             {
-        //                 // skip the duplicated-checked element
-        //                 while( (right_index > 0) && nums[right_index] == nums[right_index - 1] )
-        //                     right_index--;
-        //                 right_index--;
-        //             }
-        //         }
-        //     }
-            
-        //     return result;
-        // }
-        
-        // solution 2 (keven, time limit exceed)
+        // solution 1 (best)
         {
+            vector<vector<int>> result;  // final result
+
+            // sort the original array first
             sort(nums.begin(), nums.end());
 
-            vector<vector<int>> result;    // final result
-            set<vector<int>> database_set;   // database the single result, for judgement if the single result exist or NOT
-            
-            int index_i = 0, index_j = 0;
             int max_len = nums.size();
-            while (index_i <= max_len - 3)
+            for (size_t i = 0; i <= max_len - 3; i++)
             {
-                // skip the duplicated-checked element (REDUCE search time)
-                while( (index_i > 0) && (index_i < max_len) && nums[index_i] == nums[index_i-1] ){
-                    index_i++;
+                int num = nums[i];
+                int target = 0 - num;
+                int left_index = i + 1;
+                int right_index = max_len - 1;
+
+                // skip the duplicated-checked element
+                if( (i > 0) && nums[i] == nums[i-1] )
                     continue;
-                }
 
-                index_j = index_i + 1;
-
-                // printf("**********************\n");
-                // printf("i:%d\n", i);
-                while (index_j <= max_len - 2 /*&& index_j > index_i*/)
-                {
-                    int total_i_j = nums[index_i] + nums[index_j];
-                    int value_k = 0 - total_i_j;
-
-                    auto value_k_index_iter = nums.begin() + index_j + 1;
-                    unordered_set<int> value_k_possible_unordered_set(value_k_index_iter, nums.end());
-
-                    auto count_k = value_k_possible_unordered_set.count(value_k);
-                    // printf("nums[%d]:%d, nums[%d]:%d, value_k:%d, count(k):%d\n", i, nums[i], j, nums[j], value_k, count_k);
-                    if( count_k ){
-
-                        // BINGO~~~~ Found the answer
-                        vector<int> single_result;     // each single match result
-
-                        // record the answer
-                        single_result.push_back(nums[index_i]);
-                        single_result.push_back(nums[index_j]);
-                        single_result.push_back(value_k);
+                while( left_index < right_index ){
+                    if ( nums[left_index] + nums[right_index] == target )
+                    {
+                        result.push_back({nums[i], nums[left_index], nums[right_index]});
                         
-                        // if (database_set.count(single_result) == 0)
-                        {
-                            result.push_back(single_result);
-                            // database_set.insert(single_result);
-                        }
+                        // skip the duplicated-checked element
+                        while( (left_index + 1 < max_len) && (nums[left_index] == nums[left_index + 1]) )
+                            left_index++;
+                        // skip the duplicated-checked element
+                        while( (right_index > 0) && nums[right_index] == nums[right_index - 1] )
+                            right_index--;
 
-                        // skip the duplicated-checked element (REDUCE search time)
-                        while( (index_j+1 < max_len) && nums[index_j] == nums[index_j+1] )
-                            index_j++;
+                        left_index++;
+                        right_index--;
                     }
-                    index_j++;
+                    else if ( nums[left_index] + nums[right_index] < target )
+                    {
+                        // skip the duplicated-checked element
+                        while( (left_index + 1 < max_len) && (nums[left_index] == nums[left_index + 1]) )
+                            left_index++;
+                        left_index++;
+                    }
+                    else
+                    {
+                        // skip the duplicated-checked element
+                        while( (right_index > 0) && nums[right_index] == nums[right_index - 1] )
+                            right_index--;
+                        right_index--;
+                    }
                 }
-                // printf("**********************\n\n");
-
-                index_i++;
             }
-
+            
             return result;
         }
+        
+        // // solution 2 (keven, time limit exceed)
+        // {
+        //     sort(nums.begin(), nums.end());
+
+        //     vector<vector<int>> result;    // final result
+        //     set<vector<int>> database_set;   // database the single result, for judgement if the single result exist or NOT
+            
+        //     int index_i = 0, index_j = 0;
+        //     int max_len = nums.size();
+        //     while (index_i <= max_len - 3)
+        //     {
+        //         // skip the duplicated-checked element (REDUCE search time)
+        //         while( (index_i > 0) && (index_i < max_len) && nums[index_i] == nums[index_i-1] ){
+        //             index_i++;
+        //             continue;
+        //         }
+
+        //         index_j = index_i + 1;
+
+        //         // printf("**********************\n");
+        //         // printf("i:%d\n", i);
+        //         while (index_j <= max_len - 2 /*&& index_j > index_i*/)
+        //         {
+        //             int total_i_j = nums[index_i] + nums[index_j];
+        //             int value_k = 0 - total_i_j;
+
+        //             auto value_k_index_iter = nums.begin() + index_j + 1;
+        //             unordered_set<int> value_k_possible_unordered_set(value_k_index_iter, nums.end());
+
+        //             auto count_k = value_k_possible_unordered_set.count(value_k);
+        //             // printf("nums[%d]:%d, nums[%d]:%d, value_k:%d, count(k):%d\n", i, nums[i], j, nums[j], value_k, count_k);
+        //             if( count_k ){
+
+        //                 // BINGO~~~~ Found the answer
+        //                 vector<int> single_result;     // each single match result
+
+        //                 // record the answer
+        //                 single_result.push_back(nums[index_i]);
+        //                 single_result.push_back(nums[index_j]);
+        //                 single_result.push_back(value_k);
+                        
+        //                 // if (database_set.count(single_result) == 0)
+        //                 {
+        //                     result.push_back(single_result);
+        //                     // database_set.insert(single_result);
+        //                 }
+
+        //                 // skip the duplicated-checked element (REDUCE search time)
+        //                 while( (index_j+1 < max_len) && nums[index_j] == nums[index_j+1] )
+        //                     index_j++;
+        //             }
+        //             index_j++;
+        //         }
+        //         // printf("**********************\n\n");
+
+        //         index_i++;
+        //     }
+
+        //     return result;
+        // }
     
     }
 };
